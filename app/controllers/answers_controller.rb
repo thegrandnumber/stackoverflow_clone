b4cf885152a2e2588c9f68f4666949fbb76259fd
thegrandnumber <thegrandnumber+2@gmail.com>
2015-05-25 18:35:26 -0700

    add create route to answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,26 @@
 class AnswersController < ApplicationController
+
+    def create
+    question = Question.find(params[:question_id])
+    answer = question.answers.new(params_answers)
+
+    respond_to do |format|
+      if answer.save
+        format.html {redirect_to question_path(question)}
+        format.json {render json: answer}
+      else
+        format.html {render 'show'}
+        format.json {render json: @errors}
+      end
+    end
+    
+  end
+
+  
+
+  private
+
+  def params_answers
+    params.require(:answer).permit(:title, :content)
+  end
 end
