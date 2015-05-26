6c5a6b7bbc2e127eadca00eb3d5c3c86d16ae4a8
thegrandnumber <thegrandnumber+2@gmail.com>
2015-05-25 18:34:00 -0700

    add base answers rspec

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  before :each do
+    Question.delete_all
+    @question = Question.create!(title: "testtitle", content: "content stuff")
+    @answer = @question.answers.create(title: "Testing answer", content: "tested answer")
+  end
 
+  describe 'POST #create' do
+    before :each do
+      post :create, question_id: @question.id, answer: {title: "Testing answer", content: "tested answer"}
+    end
+
+    it "should create a new answer" do
+      expect(Answer.last.title).to eq("Testing answer")
+    end
+  end
 end
