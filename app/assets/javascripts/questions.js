2fbd8f2b9b827cb659cabc7b9086efd9690706e5
thegrandnumber <thegrandnumber+2@gmail.com>
2015-05-26 17:39:51 -0700

    add handlebars, add ajax of create Question, yet to render response on page

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,11 +1,34 @@
 $(document).ready(function() {
-  var bindEvents = function(){
-    
-  };
+    var bindEvents = function() {
+        $('#new_Q form').on('click', 'input[type="submit"]', submitNewQuestion);
+    };
+
+    var submitNewQuestion = function(event) {
+        event.preventDefault();
+        console.log("you clicked me!!!");
+        console.log($(this).parent().parent().attr("action"));
+        var url = $(this).parent().parent().attr("action");
+        var $form = $(this).parent().parent();
+        $.ajax({
+          url: url,
+          type: 'POST',
+          dataType: 'json',
+          data: $form.serialize(),
+        })
+        .done(function(response) {
+          console.log("success", response);
+          rendernewQuestion(response);
+        })
+        .fail(function(response) {
+          console.log("error", response);
+          renderErrors(response);
+        });
+        
+
+    };
 
 
+// At runtime: 
 
-  
-  bindEvents();
+    bindEvents();
 });
-
