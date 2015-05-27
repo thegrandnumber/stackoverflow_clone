c4565a868d3dcefeb70298a23995fe9877594b33
thegrandnumber <thegrandnumber+2@gmail.com>
2015-05-26 19:10:07 -0700

    add ajax render of new Q

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -18,14 +18,31 @@ $(document).ready(function() {
         .done(function(response) {
           console.log("success", response);
           rendernewQuestion(response);
+          clearForm();
         })
         .fail(function(response) {
           console.log("error", response);
-          renderErrors(response);
+          // renderErrors(response);
         });
-        
-
     };
+
+    var rendernewQuestion = function(response){
+       var html = $('#new-question-form').html();
+        var generateNewQuestionForm = Handlebars.compile(html);
+        $('#questions_box').append(generateNewQuestionForm(response));
+    };
+
+    var clearForm = function(){
+    $('#question_title').val('');
+    $('#question_content').val('');
+    };
+
+    // var renderErrors = function(response){
+    //   console.log("error");
+    //   console.log(response.responseText);
+    //   $('#errors').val('');
+    //   $('#errors').val(response[responseText]);
+    // };
 
 
 // At runtime: 
